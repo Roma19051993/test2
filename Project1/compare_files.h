8e054b4d253ef78f977dc6abd9eab0fae41ffa88
Ramzes <roma.gorbatyuk.1993@gmail.com>
2025-04-08 17:14:15 +0300

    Init commit

---
+++ Project1/compare_files.h
@@ -0,0 +1,10 @@
+#pragma once
+// compare_files.h
+#ifndef COMPARE_FILES_H
+#define COMPARE_FILES_H
+
+#include <string>
+
+void compareFiles(const std::string& file1, const std::string& file2);
+
+#endif // COMPARE_FILES_H
