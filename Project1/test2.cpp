5c994dbbcabd13b67bc3608e13c286c346515ed0
Ramzes <roma.gorbatyuk.1993@gmail.com>
2025-04-09 18:00:13 +0300

    Removed old code, rename base project file

---
+++ Project1/test2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "привіт з mybranch";
+
+    return 0;
+}
